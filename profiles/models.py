b62f2d220a353b29a2fa12f7b40c2123a59c0c10
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-06-04 21:38:46 +0200

    Add files via upload

---
+++ profiles/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from datetime import datetime, timedelta
+
+# Create your models here.
+class Profile(models.Model):
+    rank_choices = [
+        ('Bronze', 'Bronze'),
+        ('Silver', 'Silver'),
+        ('Gold', 'Gold'),
+        ('Platinum', 'Platinum'),
+        ('Diamond', 'Diamond'),
+        ('Master', 'Master'),
+        ('Predator', 'Predator'),
+    ]
+
+    type_choices = [
+        ('PS4/PS5', 'PS4/PS5'),
+        ('XBOX', 'XBOX'),
+        ('PC', 'PC'),
+    ]
+
+    country_choices = [
+        ('France', 'France'),
+        ('Belgium', 'Belgium'),
+        ('UK', 'UK'),
+        ('Spain','Spain'),
+        ('Germany','Germany'),
+        ('Italy','Italy'),
+        ('Greece','Greece'),
+    ]
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+
+    country = models.CharField(max_length=100, choices=country_choices)
+    type = models.CharField(max_length=100, choices=type_choices)
+    rank = models.CharField(max_length=100, choices=rank_choices)
+    gaming_id = models.CharField(max_length=100, null=True, blank=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/', default='default-thumbnail.jpg')
+
+    
+
+    def __str__(self):
+        return self.user.email
---
+++ users/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import User
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
