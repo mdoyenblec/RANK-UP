c008b90a116154cb6f5890dc87a1052b6c036574
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-06-21 16:10:09 +0200

    Delete chat directory

--- chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chats/', views.chat_list_view, name='chat_list'),
-    path('chats/<str:username>/', views.chat_list_view, name='chat_list'),
-]
