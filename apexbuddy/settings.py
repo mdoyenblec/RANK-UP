e03ff628609384f82eabc9ab179d46390f974b3a
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-08-05 11:04:33 +0200

    Update settings.py

--- apexbuddy/settings.py
+++ apexbuddy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$h@3bnq(lsu)ag#$yhi*_gg8i9ytgqm52jh0sowy=m+k@3d2s)"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
